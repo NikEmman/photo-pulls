d4de8a86acf4ef15f98fb188bc3dccc169e7b093
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-05-04 11:09:14 +0300

    Implement flickr api consumption

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
-    @user_id = nil
+    @user_id = params[:user_id]
+    @photos = get_user_photo_urls(@user_id) if @user_id
   end
+
+  def get_user_photo_urls(user_id)
+    flickr = Flickr.new(Rails.application.credentials.flickr[:key],
+                        Rails.application.credentials.flickr[:secret])
+
+   photos = flickr.people.getPhotos(user_id: user_id)
+   photos.map do |photo|
+        Flickr.url(photo)
+   end
+    
+  end
+
+  
 end
